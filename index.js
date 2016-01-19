19367167a1d9d22df56f8ec789c32162eb48301f
1000ch <shogo.sensui@gmail.com>
2016-01-19 23:19:38 +0900

    various tweaks

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 "use babel";
 
-import fs          from 'fs';
-import path        from 'path';
 import CSSBeautify from 'cssbeautify';
 
-export let config = {
+export const config = {
   beautifyOnSave: {
     title: 'Beautify on Save',
     description: 'Execute beautifying CSS on save.',
@@ -37,49 +35,46 @@ export let config = {
   }
 };
 
-const beautifyOnSave = () => atom.config.get('cssbeautify.beautifyOnSave');
-const indentType     = () => atom.config.get('cssbeautify.indentType')
-const indentSize     = () => atom.config.get('cssbeautify.indentSize')
-const openBrace      = () => atom.config.get('cssbeautify.openBrace')
-const autoSemicolon  = () => atom.config.get('cssbeautify.autoSemicolon')
+let beautifyOnSave;
+let indentType;
+let indentSize;
+let openBrace;
+let autoSemicolon;
 
-const getIndent = () => {
-
+function getIndent() {
   let indent = '';
-  switch (indentType()) {
+  switch (indentType) {
     case 'space':
-      indent = Array(indentSize() + 1).join(' ');
+      indent = Array(indentSize + 1).join(' ');
       break;
     case 'tab':
       indent = '\t';
       break;
   }
   return indent;
-};
+}
 
-const beautify = (css) => {
-
+function beautify(css) {
   try {
     return CSSBeautify(css, {
       indent: getIndent(),
-      openbrace: openBrace(),
-      autosemicolon: autoSemicolon()
+      openbrace: openBrace,
+      autosemicolon: autoSemicolon
     });
   } catch (e) {
     console.error(e);
   }
-
   return null;
-};
+}
 
-const execute = () => {
-
+function execute() {
   const editor = atom.workspace.getActiveTextEditor();
 
   if (!editor) {
     return;
   }
 
+  let position = editor.getCursorBufferPosition();
   let text = editor.getText();
   let selectedText = editor.getSelectedText();
 
@@ -88,32 +83,45 @@ const execute = () => {
     if (formatted) {
       let range = editor.getSelectedBufferRange();
       editor.setTextInBufferRange(range, formatted);
+      editor.setCursorBufferPosition(position);
     }
   } else {
     let formatted = beautify(text);
     if (formatted) {
       editor.setText(formatted);
+      editor.setCursorBufferPosition(position);
     }
   }
-};
+}
 
 let editorObserver = null;
 
-export const activate = (state) => {
-
+export function activate(state) {
   atom.commands.add('atom-workspace', 'cssbeautify:execute', () => {
     execute();
   });
 
-  editorObserver = atom.workspace.observeTextEditors((editor) => {
+  editorObserver = atom.workspace.observeTextEditors(editor => {
     editor.getBuffer().onWillSave(() => {
-      if (beautifyOnSave()) {
+      if (beautifyOnSave) {
         execute();
       }
     });
   });
-};
 
-export const deactivate = () => {
+  beautifyOnSave = atom.config.get('cssbeautify.beautifyOnSave');
+  indentType = atom.config.get('cssbeautify.indentType');
+  indentSize = atom.config.get('cssbeautify.indentSize');
+  openBrace = atom.config.get('cssbeautify.openBrace');
+  autoSemicolon = atom.config.get('cssbeautify.autoSemicolon');
+
+  atom.config.observe('cssbeautify.beautifyOnSave', value => beautifyOnSave = value);
+  atom.config.observe('cssbeautify.indentType', value => indentType = value);
+  atom.config.observe('cssbeautify.indentSize', value => indentSize = value);
+  atom.config.observe('cssbeautify.openBrace', value => openBrace = value);
+  atom.config.observe('cssbeautify.autoSemicolon', value => autoSemicolon = value);
+}
+
+export function deactivate() {
   editorObserver.dispose();
-};
+}
